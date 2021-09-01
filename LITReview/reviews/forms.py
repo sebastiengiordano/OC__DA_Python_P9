68c002d9b361c0ff07170a8817c238ac51aaf792
Sébastien Giordano <sebastiengiordano@yahoo.fr>
2021-09-01 18:25:18 +0200

    Add static files with the base.html for templates purpose.

--- LITReview/reviews/forms.py
+++ LITReview/reviews/forms.py
@@ -1,7 +1,17 @@
 from django import forms
 
+from django.contrib.auth.models import User
+
 
 class ConnectionForm(forms.Form):
-    user_name = forms.CharField(label='Nom d\'utilisateur', max_length=100)
+    username = forms.CharField(label='Nom d\'utilisateur', max_length=100)
     password = forms.CharField(
         label='Mot de passe', min_length=8, max_length=20)
+
+
+class RegistrationForm(forms.Form):
+    username = forms.CharField(label='Nom d\'utilisateur', max_length=100)
+    password = forms.CharField(
+        label='Mot de passe', min_length=8, max_length=20)
+    confirm_password = forms.CharField(
+        label='Confirmer mot de passe', min_length=8, max_length=20)
--- LITReview/reviews/views.py
+++ LITReview/reviews/views.py
@@ -7,7 +7,7 @@ from django.views import generic
 from django.db.models import CharField, Value
 
 from .models import Review, Ticket, UserFollows
-from .forms import ConnectionForm
+from .forms import ConnectionForm, RegistrationForm
 from .utils import (
     get_users_viewable_reviews,
     get_users_viewable_tickets
@@ -34,7 +34,30 @@ def get_connection_data(request):
     else:
         form = ConnectionForm()
 
-    return render(request, 'home_page.html', {'form': form})
+    return render(request, 'reviews/home_page.html', {'form': form})
+
+
+def get_registration_data(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = RegistrationForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect(
+                reverse(
+                    'reviews',
+                    args=(request.user, request.password))
+                )
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'reviews/home_page.html', {'form': form})
 
 
 def feed(request):
@@ -52,7 +75,7 @@ def feed(request):
         key=lambda post: post.time_created,
         reverse=True
     )
-    return render(request, 'feed.html', context={'posts': posts})
+    return render(request, 'reviews/feed.html', context={'posts': posts})
 
 
 class FeedView(generic.ListView):
